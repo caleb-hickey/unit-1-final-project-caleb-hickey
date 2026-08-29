496f3dad08cd84a960e0d31d60df948f7589b395
caleb-hickey <caleb.hickey@icloud.com>
2026-08-28 20:27:52 -0500

    adding a footer element and very basic styling

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function Footer() {
+    return(
+        <div className="footer">
+            <h4 className="footerContent">&copy; myShelf, All Rights Reserved</h4>
+        </div>
+    )
+}
+
+export default Footer;
