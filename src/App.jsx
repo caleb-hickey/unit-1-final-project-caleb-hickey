c10f93c783832db09dc477c7547bbc22fc64f25b
caleb-hickey <caleb.hickey@icloud.com>
2026-08-29 20:29:13 -0500

    implementing functionality for Browse Books component

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ return (
           <Header />
           <nav>
             <ul>
-              <li><Link to="/home">Home</Link></li>
+              <li><Link to="/">Home</Link></li>
               <li><Link to="/about">About</Link></li>
               <li><Link to="/library">Library</Link></li>
             </ul>
--- src/components/pages/BrowseBooks.jsx
+++ src/components/pages/BrowseBooks.jsx
@@ -1,12 +1,33 @@
-function BrowseBooks() {
+import BookCard from '../../assets/BookCard.jsx'
+
+function BrowseBooks ({ books, toggleOwned, toggleRead, toggleWishList }) {
     return (
         <div>
             <h1>Browse Books</h1>
+            {books.map((book) => (
+                <BookCard 
+                    key={book.id}
+                    coverUrl={book.coverUrl}
+                    title={book.title}
+                    author={book.author}
+                    genre={book.genre}
+                    pageLength={book.pageLength}
+                    id={book.id}
+                    read={book.read}
+                    owned={book.owned}
+                    wishlist={book.wishlist}
+                    toggleOwned={toggleOwned}
+                    toggleRead={toggleRead}
+                    toggleWishList={toggleWishList}
+                />
+            ))}
         </div>
     );
 }
 
-export default BrowseBooks
+export default BrowseBooks;
+
+
 
 //code from the practice-react-app project will be helpful in loading cards onto this page
 
