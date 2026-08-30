f13871d9b33f6d1c837f3fced37ccde7671d993e
caleb-hickey <caleb.hickey@icloud.com>
2026-08-29 21:49:20 -0500

    adding alt text to images

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return(
         <div className="header">
             <h1 className="pageTitle">myShelf</h1>
-            <img className="headerLogo" src={logo}></img>
+            <img className="headerLogo" alt="Logo for myShelf" src={logo}></img>
         </div>
     )
 }
